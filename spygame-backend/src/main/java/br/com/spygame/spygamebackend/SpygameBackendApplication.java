c0a0688bf9b40859e4cc47bdf6d117be5cd9b69e
Fernando Satoshi Akamine <feranando.s.akamine@gmail.com>
2023-02-25 11:35:55 -0300

    starting project

---
+++ spygame-backend/src/main/java/br/com/spygame/spygamebackend/SpygameBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.spygame.spygamebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpygameBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpygameBackendApplication.class, args);
+	}
+
+}
---
+++ spygame-backend/src/test/java/br/com/spygame/spygamebackend/SpygameBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.spygame.spygamebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpygameBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
